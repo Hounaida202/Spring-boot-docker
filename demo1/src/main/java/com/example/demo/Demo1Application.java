3115b7d14e8cb36197310ed226d496e029552208
Hounaida202 <etrichih@gmail.com>
2026-01-05 09:37:06 +0100

    tester le jenkins

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,15 @@ public class Demo1Application {
         userRepository.save(user);
         return "success";
     }
+    @GetMapping("/")
+    public String home() {
+        return "🎉 Spring Boot fonctionne dans Docker !";
+    }
+    @GetMapping("/test-ci")
+    public String testCI() {
+        return "CI/CD OK 🚀 version 2";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
